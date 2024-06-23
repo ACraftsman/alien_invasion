01b97bef608df5e7f76a13ea9d4392287e66e50d
TaiShang <1328629765@qq.com>
2024-06-23 13:48:52 +0800

    rename alien_invasion.py as main.py

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# 这是一个示例 Python 脚本。
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+    # 创建记分牌
+    sb = Scoreboard(ai_settings, screen, stats)
+    # 创建一艘飞船
+    ship = Ship(ai_settings,screen)
+    # 创建一个用于管理子弹的编组
+    bullets = Group()
+    # 创建一个管理外星人的编组
+    aliens = Group()
+    # 设置背景色(RGB)
+    bg_color = (230,230,230);
+    # 创建外星人舰队
+      # 开始游戏的主循环
+    while True:
+        # 监听键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets, stats, play_button, aliens, sb)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(bullets, aliens, ai_settings, screen, ship, stats, sb)
+            gf.update_aliens(ai_settings, aliens, ship, stats, screen, bullets, sb)
+        # 更新屏幕
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens, play_button, stats, sb)
+run_game()
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
